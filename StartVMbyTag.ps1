efc3273a4d35a35e01092338ad38251b9ba566dd
Hector Rodriguez <hector@hrod.dev>
2024-12-09 11:19:31 -0500

    Add Job Start VM by Tag

---
+++ StartVMbyTag.ps1
@@ -0,0 +1,28 @@
+﻿
+# Start VM with Tag: Automation-task-name value: autostart
+
+
+# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave -Scope Process
+
+# Connect to Azure with system-assigned managed identity
+$AzureContext = (Connect-AzAccount -Identity).context
+
+# Set and store context
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+# set and store context
+
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+# get the VM's
+$vms = Get-AzVM | Where-Object {$_.Tags['Automation-task-name'] -eq 'autostart'}
+
+# Start shutdown VM's
+foreach ($vm in $vms) {
+    $status = (Get-AzVM -Name $vm.Name - ResourceGroupName $vm.ResourceGroupName -status).statuses[1].code
+    if ($status -ne 'PowerState/running'){
+        Write-Output "Starting VM $vm.name "
+        Start-AzVM $vm.Name -ResourceGroupName $vm.ResourceGroupName -NoWait
+    }
+}
