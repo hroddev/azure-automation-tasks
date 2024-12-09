7da8db8787e0cd2e65af8ad38ab6734a7f19e31f
Hector Rodriguez <hector@hrod.dev>
2024-12-09 16:27:54 -0500

    add runbook for stop VM's

---
+++ StopVMbyTag.ps1
@@ -0,0 +1,26 @@
+﻿# Stop VM with Tag: Automation-stop-task value: autostop
+
+# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave -Scope Process
+
+# Connect to Azure with system-assigned managed identity
+$AzureContext = (Connect-AzAccount -Identity).context
+
+# Set and store context
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+# set and store context
+
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+# get the VM's
+$vms = Get-AzVM | Where-Object {$_.Tags['Automation-stop-task'] -eq 'autostop'}
+
+# Start shutdown VM's
+foreach ($vm in $vms) {
+    $status = (Get-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -status).statuses[1].code
+    if ($status -ne 'PowerState/running'){
+        Write-Output "Stoping VM $vm.name "
+        Stop-AzVM $vm.Name -ResourceGroupName $vm.ResourceGroupName -NoWait
+    }
+}
